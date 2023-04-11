6a999ec9a946794ec21b05227004608d4163283f
ᴍᴅʙᴏᴛ✅ <125588175+chamikara2006@users.noreply.github.com>
2023-04-11 06:49:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,8 +27,8 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.session = "SDA2YzlmOVE=" //session Id
+global.ownernomer = "94765837695" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
